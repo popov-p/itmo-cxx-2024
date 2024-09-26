0394427d6ca9a5da90ed6179386280212eb2414c
Pavel Popov <pashafizmat1@gmail.com>
2024-09-26 08:54:11 +0300

    report update

--- Lab-1/src/f.cpp
+++ Lab-1/src/f.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
 int main() {
-  int var_0 = 81;
-  float var_1 = 73.6043;
-  int var_2 = 50;
-  std::cout << "Result of:" << " var_2 + var_2 + var_1" << " = " <<  var_2 + var_2 + var_1 << "\n";
-  std::cout << "Result of:" << " var_0 / var_1 / var_0" << " = " <<  var_0 / var_1 / var_0 << "\n";
-  std::cout << "Result of:" << " var_0 + var_0 / var_1" << " = " <<  var_0 + var_0 / var_1 << "\n";
+  int var_0 = 77;
+  int var_1 = 71;
+  int var_2 = 35;
+  std::cout << "Result of:" << " var_2 + var_1 + var_1" << " = " <<  var_2 + var_1 + var_1 << "\n";
+  std::cout << "Result of:" << " var_0 + var_1 * var_2" << " = " <<  var_0 + var_1 * var_2 << "\n";
+  std::cout << "Result of:" << " var_0 / var_1 * var_2" << " = " <<  var_0 / var_1 * var_2 << "\n";
   return 0;
 }

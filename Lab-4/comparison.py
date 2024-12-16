c77e0a2d5fcb276588adc8903f42e62d42fec93f
Pavel Popov <pashafizmat1@gmail.com>
2024-12-16 09:31:51 +0300

    report 4 fixes

--- Lab-4/comparison.py
+++ Lab-4/comparison.py
@@ -1,17 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
 def main():
     dim_files = [(500, "build/dim_500.csv"), (1000, "build/dim_1000.csv")]
 
-    # Создаем фигуру с тремя подграфиками
     fig, axs = plt.subplots(len(dim_files), 1, figsize=(10, 18))  # 3 строки, 1 столбец
 
     for i, (dim, file) in enumerate(dim_files):
         data = pd.read_csv(file)
 
-        # Для каждого графика рисуем данные только один раз на соответствующей оси axs[i]
         axs[i].plot(data['Sparsity'], data['CustomMatrixTime'], label=f'CustomMatrix Time (dim={dim})', marker='o',
                     linestyle='-', color='b')
 
@@ -21,12 +18,12 @@ def main():
         axs[i].set_title(f'Search Time Comparison for dim={dim}')
         axs[i].set_xlabel('Sparsity')
         axs[i].set_ylabel('Search Time (seconds)')
-        axs[i].legend()  # Легенда
-        axs[i].grid(True)  # Сетка
+        axs[i].legend()
+        axs[i].grid(True)
 
-    plt.tight_layout()  # Подгонка расположения графиков
-    plt.savefig('output.png')  # Сохранение изображения
-    plt.show()  # Отображение графиков
+    plt.tight_layout()
+    plt.savefig('output.png')
+    #plt.show()
 
 
 if __name__ == "__main__":
